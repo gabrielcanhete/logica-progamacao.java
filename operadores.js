ba7b50ee7ebd8192890a5d4215c4be96aac89068
gabriel canhete alves <160603108+gabrielcanhete@users.noreply.github.com>
2024-03-14 14:27:36 +0000

    criando arquivo de logica-programacao-js

---
+++ operadores.js
@@ -0,0 +1,25 @@
+const numero1 = 20;
+const numero2 = 30;
+
+const soma = numero1 + numero2;
+
+const subtracao = numero1 - numero2;
+
+const multiplicacao = numero1 + numero2;
+
+const divisao = numero1 / numero2;
+
+const resto = numero1  % numero2;
+
+// copiar linhas: alt + shift + i
+ 
+console.log(`Os números das operacoes sao ${numero1} e ${numero2}`);
+console.log(`A soma dos números é ${soma}`);
+console.log(`A subtracao dos números é ${subtracao}`);
+console.log(`A multiplicacao dos números é ${multiplicacao}`);
+console.log(`A divisao dos números é ${divisao}`);
+console.log(`O resto da divisao dos números é ${resto}`);
+
+
+
+
